f0afa13c40992bf477ec4d79ffe4dc3e12b13e50
Audrey <aerv@ic.ufal.br>
2023-01-06 08:50:34 -0300

    update file

--- query.py
+++ query.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
+import pandas as pd
+
+smells = pd.read_csv('/Users/audreyvasconcelos/Desktop/tcc/all_smells.csv')
 
 uri = 'mongodb://localhost:27017'
 
@@ -7,12 +10,25 @@ client = MongoClient(uri)
 db = client.get_database('refactoringsdb')
 collection = db.get_collection('smells')
 
-element = 'integration.tests.JoinCollectionAndMapIT.should_persist_join_collection_and_map'
-commit_before = '429c142097888b459a68d55836506d79609c1aa1'
+print('Mongo Query...')
+appended_data = []
+i = 0
+while i < len(smells):
+    element = smells['element'][i]
+    commit_before = smells['commit_before'][i]
+    filter = {'codeElement':element, 'commit':commit_before}
+    cursor = collection.find(filter)
+    metrics = cursor[0]['metrics']
+    df_metric = pd.json_normalize(metrics) 
+    appended_data.append(df_metric)
+    i+=1
 
-filter = {'codeElement':element, 'commit':commit_before}
+appended_data = pd.concat(appended_data,ignore_index=True)
 
-cursor = collection.find(filter)
+print('Saving csv...')
+appended_data = appended_data.rename_axis('index1').reset_index()
+smells = smells.rename_axis('index1').reset_index()
 
-for each_doc in cursor:
-    print(each_doc['metrics'])
+smells_metrics = pd.merge(smells, appended_data, how = 'outer')
+smells_metrics.drop(['index1'],axis=1, inplace=True)
+smells_metrics.to_csv('/Users/audreyvasconcelos/Downloads/analises_smells/smells_metrics.csv', index=False)
